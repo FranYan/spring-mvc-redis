d7035026b65fc6fa0ca7d1da47107aace0cb51e6
FranYan <yanshihao1995@gmail.com>
2019-04-08 23:28:34 +0800

    change database

--- src/main/java/com/theo/controller/TestAdvice.java
+++ src/main/java/com/theo/controller/TestAdvice.java
@@ -1,16 +1,20 @@
 package com.theo.controller;
 
 import com.theo.util.ResultBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class TestAdvice {
 
-    @ExceptionHandler(Exception.class)
-    public ResultBean test(){
+    private static final Logger log = LoggerFactory.getLogger(TestAdvice.class);
 
-        System.out.println("11111111111111111");
+    @ExceptionHandler(Exception.class)
+    public ResultBean test(Exception e){
+
+        log.error("",e);
 
         return new ResultBean(500,"异常了，傻逼！");
     }
